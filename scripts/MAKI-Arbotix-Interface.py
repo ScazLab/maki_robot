a317cff1476d042ae8302c59bdda37508acd8e73
Kate Tsui <katherine.tsui@yale.edu>
2016-02-04 18:10:58 -0500

    [ktsui] update FEEDBACK_SC and FEEDBACK_TOPIC to include new torque feedback (TM, TL, TS). dynamically generate feedback_strings based on FEEDBACK_SC

--- scripts/MAKI-Arbotix-Interface.py
+++ scripts/MAKI-Arbotix-Interface.py
@@ -31,6 +31,10 @@ TERM_CHAR_SEND = 'Z'	## syntax for end of servo command
 SC_SET_GP = "GP"        ## servo command syntax for setting a specified servo's GOAL POSITION
 SC_SET_GS = "GS"        ## servo command syntax for setting a specified servo's GOAL SPEED
 SC_SET_IPT = "IPT"      ## servo command syntax for setting the INTERPOLATION POSE TIME
+SC_SET_TM = "TM"  	## servo command syntax for setting a specified servo's MAX TORQUE. Note: This value exists in EEPROM and persists when power is removed; default 1023
+SC_SET_TL = "TL"  	## servo command syntax for setting a specified servo's TORQUE LIMIT. Note: If set to 0 by ALARM SHUTDOWN, servo won't move until set to another value[1,1023]; default MAX TORQUE
+SC_SET_TS = "TS"  	## servo command syntax for setting a specified servo's TORQUE ENABLE. Note: Boolean [0, 1]; default 1. Doesn't appear to disable servo's movement when set to 0
+
 SC_FEEDBACK = "F"       ## servo command syntax for FEEDBACK or status of all servos
 SC_GET_MX = "MX"        ## servo command syntax for FEEDBACK of all servo MAXIMUM POSITION
 SC_GET_MN = "MN"        ## servo command syntax for FEEDBACK of all servo MINIMUM POSITION
@@ -39,6 +43,9 @@ SC_GET_PS = "PS"        ## servo command syntax for feedback with PRESENT SPEED
 SC_GET_GS = "GS"        ## servo command syntax for feedback with GOAL SPEED
 SC_GET_PT = "PT"        ## servo command syntax for feedback with PRESENT TEMPERATURE (in Celsius)
 SC_GET_PL = "PL"        ## servo command syntax for feedback with PRESENT LOAD
+SC_GET_TM = "TM"  	## servo command syntax for feedback with MAX TORQUE
+SC_GET_TL = "TL"  	## servo command syntax for feedback with TORQUE LIMIT
+SC_GET_TS = "TS"  	## servo command syntax for feedback with TORQUE ENABLE
 SC_GET_ER = "ER"        ## servo command syntax for feedback with error returned from AX_ALARM_LED
 SC_GET_DP = "DP"	## servo command syntax for default positions
 SC_GET_DS = "DS"	## servo command syntax for default speed
@@ -53,9 +60,22 @@ EP = 3  ## EYE_PAN
 ET = 4  ## EYE_TILT
 HT = 5  ## HEAD_TILT
 HP = 6  ## HEAD_PAN
+F_VAL_SEQ = [ "LR", "LL", "EP", "ET", "HT", "HP" ]
 
 ## servo control infix for type of feedback
-FEEDBACK_SC = [ SC_GET_MX, SC_GET_MN, SC_GET_PP, SC_GET_PS, SC_GET_PT, SC_GET_PL, SC_GET_ER, SC_GET_DP, SC_GET_DS ]
+FEEDBACK_SC = [ SC_GET_MX,
+		SC_GET_MN, 
+		SC_GET_PP, 
+		SC_GET_PS, 
+		SC_GET_PT, 
+		SC_GET_PL, 
+		SC_GET_ER, 
+		SC_GET_DP, 
+		SC_GET_DS,
+		SC_GET_TM,
+		SC_GET_TL,
+		SC_GET_TS
+		 ]
 FEEDBACK_TOPIC = [ "maki_feedback_max_pos",
 			"maki_feedback_min_pos",
 			"maki_feedback_pres_pos",
@@ -64,7 +84,11 @@ FEEDBACK_TOPIC = [ "maki_feedback_max_pos",
 			"maki_feedback_pres_load",
 			"maki_feedback_error",
 			"maki_feedback_default_pos",
-			"maki_feedback_default_speed" ]
+			"maki_feedback_default_speed",
+			"maki_feedback_torque_max",
+			"maki_feedback_torque_limit",
+			"maki_feedback_torque_enable"
+			]
 
 ## ---- DYNAMIC GLOBALS ---- modified programatically ----
 ALIVE = False
@@ -124,11 +148,12 @@ def recvFromArduino():
 
 def sendToMAKI (message): 
 	global maki_serial
+	global feedback_strings
+
 	maki_serial.flushOutput()
 	rospy.logdebug( "message received" + str(message) )
 	
 	#handle feedback commands
-	feedback_strings = {'FMXZ', 'FMNZ', 'FPPZ', 'FPSZ', 'FPTZ', 'FPLZ', 'FERZ', 'FDPZ', 'FDSZ'}
 	if message.data in feedback_strings:
 		feedback(message.data);
 		return
@@ -168,15 +193,23 @@ def initPubFeedback():
 	#_fname = initPubFeedback.__name__	## same as above but less modular
 
 	global feedback_pub_dict, feedback_topic_name_dict
+	global feedback_strings
 	global FEEDBACK_SC, FEEDBACK_TOPIC
 	rospy.logdebug( "setup rostopic publishers to give feedback" )
 			
 	feedback_topic_name_dict = dict( zip(FEEDBACK_SC, FEEDBACK_TOPIC) )
 	#print feedback_topic_name_dict	## debugging
 	feedback_pub_dict = { }		# init as empty dictionary
+	feedback_strings = []		# init as empty list
 	for _sc_dict_key, _feedbackTopic in feedback_topic_name_dict.iteritems():
 		_pub = rospy.Publisher(_feedbackTopic, String, queue_size = 26, latch = True)	## any new subscribers will see the most recent message published
 		feedback_pub_dict[_sc_dict_key] = _pub
+
+		## dynamically generate feedback_strings based on FEEDBACK_SC
+		feedback_strings.append( str(SC_FEEDBACK + _sc_dict_key + TERM_CHAR_SEND) )
+		#feedback_strings = {'FMXZ', 'FMNZ', 'FPPZ', 'FPSZ', 'FPTZ', 'FPLZ', 'FERZ', 'FDPZ', 'FDSZ'}
+
+	#print feedback_strings
 	return
 
 def initFeedbackFormat():
