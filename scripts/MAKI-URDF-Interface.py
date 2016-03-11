60f40a71e1c4157cabd5c59890b866fd5c7f99d7
Kate Tsui <katherine.tsui@yale.edu>
2016-03-11 13:22:59 -0500

    [ktsui] now given more complex URDF model of MAKI, don't assume that JointStates 0-5 are the ones to publish to /maki_command. Match headers with JointState names.

--- scripts/MAKI-URDF-Interface.py
+++ scripts/MAKI-URDF-Interface.py
@@ -22,29 +22,59 @@ def sendCommand(jointState):
     global defVals
     global headers
     global INIT
+    global map_js_to_headers
 
-    rospy.loginfo( jointState )
+    #rospy.logdebug( jointState )
     if INIT:
-        ## TODO: match headers with JointState
-        ## Below assumed that JointState 0-5 match the headers in order defined above
+        ## Match headers with JointState
+        map_js_to_headers = list()	## initialize to empty list
+        for i in range(0, len(headers)):
+            index = None
+            for j in range(0, len(jointState.name)):
+                if (headers[i] == "HT" and jointState.name[j] == "head_tilt"):
+		    index = j
+		    break
+		elif (headers[i] == "HP" and jointState.name[j] == "head_pan"):
+		    index = j
+		    break
+		elif (headers[i] == "EP" and jointState.name[j] == "eye_right_pan"):
+		    index = j
+		    break
+		elif (headers[i] == "ET" and jointState.name[j] == "eye_right_tilt"):
+		    index = j
+		    break
+		elif (headers[i] == "LL" and jointState.name[j] == "eyelid_left_joint"):
+		    index = j
+		    break
+		else:
+		    pass
+
+	    if index != None:
+		#rospy.logdebug( index )
+		map_js_to_headers.append(index)
         INIT = False
+    #rospy.logdebug( map_js_to_headers )
 
     command = ""
     comFlag = 0
-    for i in range(0, 5):
-        servoVal = int(defVals[i] - jointState.position[i]/0.00511326171)
+    i = 0
+    for index in map_js_to_headers:
+        servoVal = int(defVals[i] - jointState.position[index]/0.00511326171)
         if (curVals[i] != servoVal):
             comFlag = 1
             command += headers[i] + "GP" + str(servoVal)
             curVals[i] = servoVal
+        i += 1
     if (comFlag):
         pub = rospy.Publisher("maki_command", String, queue_size = 26)
         pub.publish(command + "Z")
         comFlag = 0
+        rospy.logdebug( command )
         
 
 if __name__ == '__main__':
     global INIT
+    global map_js_to_headers
     INIT = True
-
+    map_js_to_headers = list()	## initialize to empty list
     listener()
