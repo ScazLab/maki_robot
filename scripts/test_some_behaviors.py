e3be65fbbe2209b78a53d0f89688083b57f5033d
JakeBrawer <jake.brawer@yale.edu>
2019-04-05 17:10:06 -0400

    Node+launchfile to quickly test a few behaviors.

---
+++ scripts/test_some_behaviors.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+import rospy
+from std_msgs.msg import String
+
+
+if __name__ == '__main__':
+    rospy.init_node("test_behaviors")
+    rospy.loginfo("Sending test commands")
+    pub = rospy.Publisher("/maki_macro", String, queue_size=10)
+    msgs = [
+        "reset eyelids" ,
+        "spontaneousBlink start",
+        "nod"
+    ]
+
+    for msg in msgs:
+        rospy.loginfo(msg)
+        pub.publish(String(msg))
+        rospy.sleep(2.0)
